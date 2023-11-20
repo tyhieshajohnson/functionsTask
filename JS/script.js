5555047a8ce5387bec8a6b52e46775e72f5892a7
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-20 10:29:46 +0200

    q3

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,21 @@
-// Create a function called "greet" that accepts a single parameter. Log the parameter along with a "Hello".
+// Create a function called "greet" that accepts a single parameter. 
+// Log the parameter along with a "Hello".
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+greet('Tyhiesha');
 // Create a function called multiply. It should accept two parameters and log the multiplication of the two given parameters.
-// Create a function called tableOfFive which accepts a single parameter. Return the parameter multiplied by five to a variable, and log this variable.
+function multiply(x,y){
+    let answer= x*y;
+    console.log(multiply);
+    return x*y;
+}
+multiply(6,7);
+// Create a function called tableOfFive which accepts a single parameter.
+// Return the parameter multiplied by five to a variable, and log this variable.
+function tableOfFive(value) {
+    let answer= value * 5;
+    console.log(answer);
+    return answer;
+}
 // Please take note of the difference between log and return.
