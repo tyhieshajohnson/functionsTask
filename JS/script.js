fcb3366f574ee4cc11e589c372235f4f8e533acc
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-20 09:49:40 +0200

    initialCommit

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+// Create a function called "greet" that accepts a single parameter. Log the parameter along with a "Hello".
+// Create a function called multiply. It should accept two parameters and log the multiplication of the two given parameters.
+// Create a function called tableOfFive which accepts a single parameter. Return the parameter multiplied by five to a variable, and log this variable.
+// Please take note of the difference between log and return.
